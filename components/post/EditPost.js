d4a80b47016dfc35b019b5f4d24f0ca04f4c7b7a
fghovde <fghovde@gmail.com>
2023-05-23 15:59:01 +0200

    Launch 1.3 - Animation

--- components/post/EditPost.js
+++ components/post/EditPost.js
@@ -17,7 +17,7 @@ const GET_POSTS = gql`
 
 const GET_AVFALLSMERKER = gql`
 query GetAvfallsmerker {
-  posts(where: {categoryName: "avfallsmerke"}) {
+  posts(first: 50, where: {categoryName: "avfallsmerke"}) {
     nodes {
       id
       title
@@ -57,17 +57,25 @@ export function EditPost() {
     });
   }
 
+  const handleClick = (event) => {
+    const img = event.target;
+    img.classList.add('animate-spin');
+    setTimeout(() => {
+      img.classList.remove('animate-spin');
+    }, 15000);
+  };
+
   return (
     <ul className='w-80'>
       {data.posts.nodes.map((post) => (
         <li className='grid grid-cols-12 p-1 items-center' key={post.id}>
           <h3 className='text-xl text-center font-bold pt-1'>{post.title}</h3>
-          <form className='col-span-10 items-center' onSubmit={(e) => {
+          <form className='grid grid-flow-col items-center' onSubmit={(e) => {
             e.preventDefault();
             handleUpdatePost(post.id, e.target.avfallsmerker.value);
           }}>
             <select 
-              className='w-auto p-1 mx-2 rounded-md border-2 border-sky-950 hover:border-teal-600 focus:outline-none' 
+              className='h-8 w-60 p-1 mx-2 rounded-md border-2 border-sky-950 hover:border-teal-600 focus:outline-none' 
               name="avfallsmerker"
             >
               <option className='text-gray-400' value="" defaultValue>Velg avfallstype</option>
@@ -76,10 +84,10 @@ export function EditPost() {
               ))}
             </select>
             <button
-              className='p-1 text-white rounded-md bg-sky-950 hover:bg-teal-800 active:bg-teal-700 focus:outline-none focus:bg-teal-700'
+              className='h-8 w-8 p-2 text-white rounded-md bg-sky-950 hover:bg-teal-800 active:bg-teal-700 focus:outline-none focus:bg-teal-700'
               type="submit"
             >
-              <img src='/update.png' width='20px' height='20px' />
+              <img onClick={handleClick} className='' src='/update.png' width='20px' height='20px' />
             </button>
           </form>
         </li>
--- pages/rir/dashboard.js
+++ pages/rir/dashboard.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import { CreatePost } from '../../components/post/CreatePost'
 import { EditPost } from '../../components/post/EditPost'
 import { Layout } from '../../components/layout/Layout'
-import { SignOut }  from '../../components/login/SignOut'
 
 export default function Dashboard() {
   return (
@@ -21,9 +20,6 @@ export default function Dashboard() {
         <div>
             <h2 className='pb-4 text-2xl font-semibold text-center'>Skjermer</h2>
             <EditPost />
-        </div>
-        <div>
-          <SignOut />
         </div>
       </Layout>
     </>
